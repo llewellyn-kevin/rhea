2174b157afb703fc1e433efe96569d11cb7954fb
llewellyn-kevin <llewellyn.kevin@yahoo.com>
2019-03-30 11:51:37 +0000

    Added packages for password hashing and added unique constraint to username

--- rhea/lib/rhea/user.ex
+++ rhea/lib/rhea/user.ex
@@ -1,6 +1,7 @@
 defmodule Rhea.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Comeonin.Bcrypt
 
   schema "users" do
     field :password, :string
@@ -13,6 +14,8 @@ defmodule Rhea.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:username, :password])
+    |> unique_constraint(:username)
     |> validate_required([:username, :password])
+    |> update_change(:password, &Bcrypt.hashpwsalt/1)
   end
 end
